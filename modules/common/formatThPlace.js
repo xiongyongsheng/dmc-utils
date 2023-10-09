6b120447e323ccbb3be3b948ea59020ff3dff2d9
xiongyongsheng <xiongys@humming-tech.com>
2023-10-09 19:37:47 +0800

    修改:添加formatThPlace

--- modules/common/formatThPlace.js
+++ modules/common/formatThPlace.js
@@ -1,6 +1,6 @@
 export default function formatThPlace(number) {
   const num = Number(number);
-  if (!isNaN(num)) return number;
+  if (isNaN(num)) return number;
   if (num >= 10000) {
     return `${num / 10000}万`;
   } else {
