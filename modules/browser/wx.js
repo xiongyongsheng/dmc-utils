f312c3b1156afe86eb38fd3b9223099e1c36de6e
xiongyongsheng <xiongys@humming-tech.com>
2023-11-02 15:19:43 +0800

    update: wx.js add hideMenu function

--- modules/browser/wx.js
+++ modules/browser/wx.js
@@ -4,6 +4,24 @@ export function isWeiXin() {
 export function isQYWeiXin() {
   return navigator.userAgent.includes("wxwork");
 }
+// 隐藏浏览器菜单栏
+export function hideMenu() {
+  if (typeof WeixinJSBridge == "undefined") {
+    if (document.addEventListener) {
+      document.addEventListener("WeixinJSBridgeReady", onBridgeReady, false);
+    } else if (document.attachEvent) {
+      document.attachEvent("WeixinJSBridgeReady", onBridgeReady);
+      document.attachEvent("onWeixinJSBridgeReady", onBridgeReady);
+    }
+  } else {
+    onBridgeReady();
+  }
+}
+
+function onBridgeReady() {
+  WeixinJSBridge.call("hideOptionMenu");
+}
+
 export function wxAuth({
   appId,
   corpId,
